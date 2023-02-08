a7478bc07cb12c201711d7608963cb10f79699ae
Fadhilkgm <fadhilkgm64@gmail.com>
2023-02-08 12:02:07 +0530

    nav,hero,and about complete

--- client/app/src/App.js
+++ client/app/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
-import Nav from './components/Headers/Nav';
+import Nav from './components/Navbar/Nav';
+import About from './components/About/About';
 
 function App() {
   return (
     <div className="App">
       <Nav />
+      <About />
     </div>
   );
 }
